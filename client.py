dd00b2d87c2b738d9ff4b843fe5bdda546b5bfc8
farkon00 <sammer2016sammer@gmail.com>
2022-04-07 12:19:08 +0300

    Client added, get method added

---
+++ client.py
@@ -0,0 +1,43 @@
+import requests
+
+class Client:
+    "Client for freelancehunt api v2"
+
+    URL = "https://api.freelancehunt.com/v2/"
+
+    def __init__(self, key: str, currency: str = "UAH", language: str = "ru"):
+        self._key = key
+
+        self.currency = currency
+        self.language = language
+
+    @property
+    def key(self):
+        raise AttributeError("Key is secret")
+
+    @key.setter
+    def key(self, value):
+        self._key = value
+
+    def _value_to_param(self, value: object) -> None:
+        """Converts value to param for requests"""
+
+        if value is None:
+            return "null"
+
+        if isinstance(value, str):
+            return value
+
+        if isinstance(value, int):
+            return str(value)
+
+        if isinstance(value, bool):
+            return str(int(value))
+
+    def get(self, url: str, headers : dict[str : object] = {}, **kwargs):
+        """Get request method for freelancehunt api"""
+        return requests.get(
+            Client.URL + url,
+            headers={**headers, "Authorization" : f"Bearer {self._key}"}, 
+            params={i : self._value_to_param(j) for i, j in kwargs.items()}
+        ).json()
